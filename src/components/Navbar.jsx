e67b2b0200b7993f11122d22ac1c153ffb749284
Jeevant Singh <anupamasinghsrinet1976@gmail.com>
2025-11-22 09:07:41 +0530

    dock functionality added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,31 @@
-import dayjs from "dayjs"
+import dayjs from "dayjs";
 
-import { navLinks, navIcons } from "#constants"
+import { navLinks, navIcons } from "#constants";
 
 export const Navbar = () => {
   return (
     <nav>
-        <div>
-            <img src="/images/logo.svg" alt="logo" />
-            <p className="font-bold text-sm"> Jeevant's Portfolio</p>
-            <ul>
-                {navLinks.map(({ id , name}) =>(
-                    <li key={id}>
-                        <p>{name}</p>
-                        </li>
-                ))}
-            </ul>
-        </div>
-        <div>
-            <ul>
-                {navIcons.map(({id , img}) => (
-                    <li key={id}>
-                        <img src={img} className="icon-hover" alt ={'icon-${id}'}></img>
-                    </li>
-                )) }
-            </ul>
-                <time>{dayjs().format("ddd MMM D h:mm A")}</time>
-        </div>
+      <div>
+        <img src="/images/logo.svg" alt="logo" />
+        <p className="font-bold text-sm"> Jeevant's Portfolio</p>
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} className="icon-hover" alt={"icon-${id}"}></img>
+            </li>
+          ))}
+        </ul>
+        <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+      </div>
     </nav>
-  )
-}
+  );
+};
